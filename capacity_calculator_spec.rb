6cd8b1022d6b96bf0c4e49b8c21230e4edf11b5f
Menyhert Fatyol <mfatyol@emarsys.com>
2018-10-29 16:49:01 +0100

    fix typo

--- capacity_calculator_spec.rb
+++ capacity_calculator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CapacityCalculator do
     context "When working on full capacity" do
 
       cases = [
-          {description: "" pages: 1000, date: "2018-06-11 16:00", result: 1000}
+          {description: "", pages: 1000, date: "2018-06-11 16:00", result: 1000}
       ]
 
       it 'returns the maximum amount of pages per hour' do
