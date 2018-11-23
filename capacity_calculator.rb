6b6184e3632b3ebbcd37a8629f0e2b5f34b2d625
Menyhert Fatyol <mfatyol@emarsys.com>
2018-11-23 16:31:49 +0100

    adds logic to be closed on weekends as per the documentation

--- capacity_calculator.rb
+++ capacity_calculator.rb
@@ -4,7 +4,7 @@ class CapacityCalculator
 
   def calculate(pages_per_hour, date)
     iso_date = DateTime.parse date
-    return "The press is open between 9AM - 5PM on weekdays" if iso_date.hour < 9 or iso_date.hour > 17
+    return "The press is open between 9AM - 5PM on weekdays" if (iso_date.hour < 9 or iso_date.hour > 17) or (iso_date.saturday? or iso_date.sunday?)
 
     self.pages_by_hour(pages_per_hour, iso_date.hour)
 
