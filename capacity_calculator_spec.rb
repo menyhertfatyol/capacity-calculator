0183311c201b5ec79c5c6045039b9845a4407726
Menyhert Fatyol <mfatyol@emarsys.com>
2018-11-23 16:32:34 +0100

    refactor tests by parametrising

--- capacity_calculator_spec.rb
+++ capacity_calculator_spec.rb
@@ -2,38 +2,30 @@ require_relative 'capacity_calculator'
 
 RSpec.describe CapacityCalculator do
   describe '#calculate' do
-    context "When working on full capacity" do
 
+    context "On a usual workday" do
       cases = [
-          {description: "", pages: 1000, date: "2018-06-11 16:00", result: 1000}
+          {description: "when working on full capacity", pages: 1000, date: "2018-06-11 16:00", result: 1000},
+          {description: "when working on 25% from the first hour", pages: 2000, date: "2018-06-12 09:00", result: 500},
+          {description: "when working on 50% from the secound hour", pages: 3000, date: "2018-10-18 10:00", result: 1500},
+          {description: "when working on 75% from the third hour", pages: 4000, date: "2018-10-18 11:00", result: 3000},
+          {description: "When press is not open", pages: 4000, date: "2018-10-18 11:00", result: 3000},
       ]
 
-      it 'returns the maximum amount of pages per hour' do
-        expect(CapacityCalculator.new.calculate(1000, "2018-06-11 16:00")).to eq(1000)
+      cases.each do |test_case|
+        it "returns #{test_case[:result]} out of #{test_case[:pages]} pages #{test_case[:description]}" do
+          expect(CapacityCalculator.new.calculate(test_case[:pages], test_case[:date])).to eq(test_case[:result])
+        end
       end
     end
 
-    context "When printers are warming up" do
-      it 'works on 25% capacity in the first hour' do
-        expect(CapacityCalculator.new.calculate(2000, "2018-06-12 09:00")).to eq(500)
-      end
-
-      it 'works on 50% capacity from the secound hour' do
-        expect(CapacityCalculator.new.calculate(3000, "2018-10-18 10:00")).to eq(1500)
-      end
-
-      it 'works on 75% capacity from the third hour' do
-        expect(CapacityCalculator.new.calculate(4000, "2018-10-18 11:00")).to eq(3000)
-      end
-    end
-    
     context "When press is not open" do
       it 'returns a message about the opening hours' do
         expect(CapacityCalculator.new.calculate(1000, "2018-10-18 07:00")).to eq("The press is open between 9AM - 5PM on weekdays")
       end
     end
   end
-  
+
   describe '#is_last_friday?' do
     it 'returns true if date is fryday and is the last friday of the month' do
       expect(CapacityCalculator.new.is_last_friday?(Date.parse "2018-10-26")).to be_truthy
